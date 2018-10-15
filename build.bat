156835abb4700dadba94634b566afe6eeaea51d6
Unknown <monamimani@gmail.com>
2018-10-15 13:40:47 -0400

    Gitignore, pipeline and buid
    - removed commented task from azure-pipelines.yml
    - Addded gitignore to ignore build and install folder
    - cleanning build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-ECHO ON
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-call "vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 set cc=cl
 set cxx=cl
